fbb5b75a5dbd8c0b004c23ef8700f8c24131e9ef
Samba Diallo <samba.diallo@id-mac007.idtgv.cnit>
2020-11-06 16:40:45 +0100

    different react redux actions

---
+++ src/components/ErrorGenerator.js
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+const ErrorGenerator = ({ action ,  value}) => {
+    console.log("valeur du value", value)
+ return (
+    <p>
+      <Button variant="primary" style={{marginBottom:'5px', marginTop:'15px'}} onClick={() => action('ACTION_ERROR_IN_PUT')}>Action error in put :  </Button>{' '} <br/>
+      <Button variant="secondary" style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_SELECT')}>Action error in select</Button>{' '}<br/>
+      <Button variant="success"  style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_CALL_SYNC')}>Action error in call sync</Button>{' '}<br/>
+      <Button variant="warning" style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_CALL_ASYNC')}>Action error in call async</Button>{' '}<br/>
+      <Button variant="danger" style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_CALL_INLINE')}>Action error in call inline</Button>{' '}<br/>
+      <Button variant="light"  style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_FORK')}>Action error in fork</Button>{' '}<br/>
+      <Button variant="link" style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_SPAWN')}>Action error in spawn</Button>{' '}<br/>
+      <Button variant="primary" style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_RACE')}>Action error in race</Button>{' '}<br/>
+      <Button variant="secondary" style={{marginBottom:'5px'}} onClick={() => action('ACTION_CAUGHT_ERROR')}>Action caught error</Button>{' '}<br/>
+      <Button variant="success" style={{marginBottom:'5px'}} onClick={() => action('ACTION_INLINE_SAGA_ERROR')}>Action error in inlined saga</Button>{' '}<br/>
+      <Button variant="danger" style={{marginBottom:'5px'}}  onClick={() => action('ACTION_IN_DELEGATE_ERROR')}>Action error in delegated saga</Button>{' '}<br/>
+      <Button variant="secondary" style={{marginBottom:'5px'}} onClick={() => action('ACTION_FUNCTION_EXPRESSION_ERROR')}>
+        Action error in saga as function expression
+      </Button><br/>
+      <Button variant="warning"  style={{marginBottom:'5px'}} onClick={() => action('ACTION_ERROR_IN_RETRY')}>Error in retry</Button><br/>
+      <Button variant="secondary" onClick={() => action('ACTION_ERROR_PRIMITIVE')}>Error as a primitive</Button><br/>
+    </p>
+  )
+}
+
+ErrorGenerator.propTypes = {
+  action: PropTypes.func.isRequired,
+}
+
+export default ErrorGenerator
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ErrorGenerator from './components/ErrorGenerator'
+import reducer from './reducers'
+import rootSaga from './sagas'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+sagaMiddleware.run(rootSaga)
+
+const action = type => store.dispatch({ type })
+
+ReactDOM.render(<ErrorGenerator value={store.getState()} action={action} />, document.getElementById('root'));
+
+
 reportWebVitals();
